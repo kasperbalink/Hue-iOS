767ad9d203e290eb0288d919602cfbb10d8490c6
Kasper Balink <kasperbalink@Kaspers-Mac.local>
2016-09-30 00:23:24 -0700

    Initial Commit

---
+++ Philips Hue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Philips Hue
+//
+//  Created by Kasper Balink on 30/09/2016.
+//  Copyright © 2016 Kasper Balink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
